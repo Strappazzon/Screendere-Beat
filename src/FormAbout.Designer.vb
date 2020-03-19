c0cf28d15f2dc251880c0d755822179f9fe525fe
Strappazzon <strappazzon@protonmail.com>
2020-03-19 22:26:28 +0100

    Credit folder icon author

--- src/FormAbout.Designer.vb
+++ src/FormAbout.Designer.vb
@@ -39,7 +39,7 @@ Partial Class FormAbout
         Me.AboutLabel.ForeColor = System.Drawing.Color.White
         Me.AboutLabel.Location = New System.Drawing.Point(12, 50)
         Me.AboutLabel.Name = "AboutLabel"
-        Me.AboutLabel.Size = New System.Drawing.Size(391, 90)
+        Me.AboutLabel.Size = New System.Drawing.Size(391, 120)
         Me.AboutLabel.TabIndex = 0
         Me.AboutLabel.Text = resources.GetString("AboutLabel.Text")
         '
@@ -61,7 +61,7 @@ Partial Class FormAbout
         Me.HomepageLabel.Cursor = System.Windows.Forms.Cursors.Hand
         Me.HomepageLabel.Font = New System.Drawing.Font("MS Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.HomepageLabel.ForeColor = System.Drawing.Color.White
-        Me.HomepageLabel.Location = New System.Drawing.Point(14, 155)
+        Me.HomepageLabel.Location = New System.Drawing.Point(14, 185)
         Me.HomepageLabel.Name = "HomepageLabel"
         Me.HomepageLabel.Size = New System.Drawing.Size(113, 12)
         Me.HomepageLabel.TabIndex = 3
@@ -74,7 +74,7 @@ Partial Class FormAbout
         Me.SupportLabel.Cursor = System.Windows.Forms.Cursors.Hand
         Me.SupportLabel.Font = New System.Drawing.Font("MS Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.SupportLabel.ForeColor = System.Drawing.Color.White
-        Me.SupportLabel.Location = New System.Drawing.Point(14, 175)
+        Me.SupportLabel.Location = New System.Drawing.Point(14, 205)
         Me.SupportLabel.Name = "SupportLabel"
         Me.SupportLabel.Size = New System.Drawing.Size(59, 12)
         Me.SupportLabel.TabIndex = 4
@@ -87,7 +87,7 @@ Partial Class FormAbout
         Me.ChangelogLabel.Cursor = System.Windows.Forms.Cursors.Hand
         Me.ChangelogLabel.Font = New System.Drawing.Font("MS Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ChangelogLabel.ForeColor = System.Drawing.Color.White
-        Me.ChangelogLabel.Location = New System.Drawing.Point(330, 155)
+        Me.ChangelogLabel.Location = New System.Drawing.Point(330, 185)
         Me.ChangelogLabel.Name = "ChangelogLabel"
         Me.ChangelogLabel.Size = New System.Drawing.Size(71, 12)
         Me.ChangelogLabel.TabIndex = 5
@@ -100,7 +100,7 @@ Partial Class FormAbout
         Me.LicenseLabel.Cursor = System.Windows.Forms.Cursors.Hand
         Me.LicenseLabel.Font = New System.Drawing.Font("MS Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LicenseLabel.ForeColor = System.Drawing.Color.White
-        Me.LicenseLabel.Location = New System.Drawing.Point(342, 175)
+        Me.LicenseLabel.Location = New System.Drawing.Point(342, 205)
         Me.LicenseLabel.Name = "LicenseLabel"
         Me.LicenseLabel.Size = New System.Drawing.Size(59, 12)
         Me.LicenseLabel.TabIndex = 6
@@ -111,7 +111,7 @@ Partial Class FormAbout
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(414, 201)
+        Me.ClientSize = New System.Drawing.Size(414, 231)
         Me.Controls.Add(Me.LicenseLabel)
         Me.Controls.Add(Me.ChangelogLabel)
         Me.Controls.Add(Me.SupportLabel)

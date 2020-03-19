a1aa6edae5ade8d2b6c248f7c04c4e9bd8582d45
Strappazzon <strappazzon@protonmail.com>
2020-03-19 21:24:25 +0100

    Create settings file if it doesn't exist

--- src/Settings.vb
+++ src/Settings.vb
@@ -11,19 +11,31 @@ Public Class Settings
             Form1.UIDTextBox.Text = SteamID()
             Form1.SoundChkBox.Checked = PlaySound()
         Else
+            'Create directory
+            Directory.CreateDirectory(SettingsFile.Replace("\settings.ini", ""))
+
+            'Create settings
             Dim Data As IniData = New IniData()
             Data("screendere")("steamid") = Nothing
             Data("screendere")("playsound") = False
 
+            'Write settings to file
             File.WriteAllText(SettingsFile, Data.ToString())
         End If
     End Sub
 
     Public Shared Sub SaveSettings()
+        'Add settings to IniData
         Dim Data As IniData = New IniData()
         Data("screendere")("steamid") = Form1.UIDTextBox.Text
         Data("screendere")("playsound") = Form1.SoundChkBox.Checked
 
+        'Create directory if it's been deleted
+        If Not Directory.Exists(SettingsFile.Replace("\settings.ini", "")) Then
+            Directory.CreateDirectory(SettingsFile.Replace("\settings.ini", ""))
+        End If
+
+        'Write settings to file
         File.WriteAllText(SettingsFile, Data.ToString())
     End Sub
 
